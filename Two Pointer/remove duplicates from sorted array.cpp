a33174d208b030547a458978fb292d7825e5f2c6
AtuL-KumaR-00 <atulcrj00@gmail.com>
2022-06-11 10:08:58 +0530

    Create remove duplicates from sorted array.cpp

---
+++ Two Pointer/remove duplicates from sorted array.cpp
@@ -0,0 +1,8 @@
+int removeDuplicates(vector<int> &arr, int n) {
+	// Write your code here.
+    int ans=0;
+    for(int i=1;i<n;i++){
+        if(arr[i]!=arr[i-1])    ans++;    
+    }
+    return ans+1;
+}
