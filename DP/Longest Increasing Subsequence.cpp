fbd46b47f17a3f13d723902e9ed50feb86820ab5
AtuL-KumaR-00 <atulcrj00@gmail.com>
2022-06-28 23:15:59 +0530

    Update Longest Increasing Subsequence.cpp

--- DP/Longest Increasing Subsequence.cpp
+++ DP/Longest Increasing Subsequence.cpp
@@ -45,3 +45,29 @@ int longestIncreasingSubsequence(int arr[], int n)
     vector<vector<int>> dp(n+2,vector<int>(n+2,-1));
     return f(0,-1,arr,n,dp);
 }
+
+Approach 3:
+
+#include<bits/stdc++.h>
+//int count=0;
+int f(int idx,int prevIdx,int arr[], int n,vector<vector<int>>&dp){
+    if(idx==n)    return 0;
+    int len=0;
+    if(dp[idx+2][prevIdx+1]!=-1)    len=dp[idx+2][prevIdx+1];
+    else {
+        dp[idx+2][prevIdx+1] = 0+f(idx+1,prevIdx,arr,n,dp);
+        len=dp[idx+2][prevIdx+1];
+    }
+    if(prevIdx==-1 || arr[idx]>arr[prevIdx]){
+        if(dp[idx+2][idx+1] ==-1)
+            dp[idx+2][idx+1] = 1+f(idx+1,idx,arr,n,dp);
+        len=max(len, dp[idx+2][idx+1]);
+    }
+    return dp[idx+1][prevIdx+1]=len;
+}
+
+int longestIncreasingSubsequence(int arr[], int n)
+{
+    vector<vector<int>> dp(n+2,vector<int>(n+2,-1));
+    return f(0,-1,arr,n,dp);
+}
