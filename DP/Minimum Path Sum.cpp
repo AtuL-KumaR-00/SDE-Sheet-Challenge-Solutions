ffd8c6cf238545f8b525af324d63b6d93945c3e0
AtuL-KumaR-00 <atulcrj00@gmail.com>
2022-06-29 21:37:47 +0530

    Update Minimum Path Sum.cpp

--- DP/Minimum Path Sum.cpp
+++ DP/Minimum Path Sum.cpp
@@ -13,3 +13,32 @@ int minSumPath(vector<vector<int>> &grid) {
     vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
     return f(grid,grid.size()-1,grid[0].size()-1,dp);
 }
+
+
+APPROACH 2:
+
+#include<bits/stdc++.h>
+int minSumPath(vector<vector<int>> &grid) {
+    vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),0));
+    int n=grid.size();
+    int m=grid[0].size();
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(i==0 && j==0)    dp[i][j]=grid[i][j];
+            else  {
+                int down=grid[i][j];
+                if(i>0)
+                    down+=dp[i-1][j];
+                else down+=1e9;
+                
+                int right=grid[i][j];
+                if(j>0)
+                    right+=dp[i][j-1];
+                else right+=1e9;
+                
+                dp[i][j]=min(down,right);
+            }
+        }
+    }
+    return dp[n-1][m-1];
+}
