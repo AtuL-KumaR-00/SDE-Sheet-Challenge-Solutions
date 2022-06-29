480018c509751c84651839d2da8e610b4c184b97
AtuL-KumaR-00 <atulcrj00@gmail.com>
2022-06-29 09:30:45 +0530

    Update 0 1 Knapsack.cpp

--- DP/0 1 Knapsack.cpp
+++ DP/0 1 Knapsack.cpp
@@ -24,3 +24,20 @@ int maxProfit(vector<int> &values, vector<int> &weights, int n, int w)
 
 APPROACH 2:
 
+#include<bits/stdc++.h>
+int f(vector<int> &values, vector<int> &weights, int n, int w, vector<vector<int>>&dp){
+    if(n==0 || w==0)    return 0;
+    if(dp[n][w]!=-1)    return dp[n][w];
+    if(weights[n-1]<=w){
+        return dp[n][w]=max(values[n-1] + f(values,weights,n-1,w-weights[n-1],dp), f(values,weights,n-1,w,dp));
+    }else{
+        return dp[n][w]=f(values,weights,n-1,w,dp);
+    }
+}
+
+int maxProfit(vector<int> &values, vector<int> &weights, int n, int w)
+{
+    vector<vector<int>> dp(n+1,vector<int>(w+1,-1));
+    return f(values,weights,n,w,dp);
+}
+
